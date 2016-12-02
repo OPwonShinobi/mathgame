4b461da495dc9f5e0488bc95adc2ceaa7845391c
Alex Xia <zenmully@gmail.com>
2016-12-01 22:55:54 -0800

    first commit

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>Math Game</title>
+		<meta charset="utf-8">
+		<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" type="text/css" href="styles/stylesheet.css">
+	</head>
+	<body>
